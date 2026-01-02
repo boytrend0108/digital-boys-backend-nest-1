4643957e114682e6e3e1ec449d6867bc503d85fe
boytrend0108 <boytrend0108@users.noreply.github.com>
2026-01-02 07:38:29 +0200

    feat: creage a postrges db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TaskModule, MovieModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'root',
+      password: '123456',
+      database: 'nestdb',
+      entities: [],
+      synchronize: true,
+    }),
+    TaskModule,
+    MovieModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
